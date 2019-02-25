eca81527562e99014b4dcea3c4b887ce8d263f9d
Yuntao Ma <mayuntao94@gmail.com>
2019-02-25 18:56:20 +0100

    added actor_critic for cart-pole and modified learning rate for pong

--- atari/tf_cart_ac.py
+++ atari/tf_cart_ac.py
@@ -4,18 +4,18 @@ import gym
 import time
 
 # model params
-input_shape = [80, 80, 1]
+input_shape = [4]
 output_shape = [2]
-hidden_units = 256
-learning_rate = 1e-3
+hidden_units = 10
+learning_rate = 1e-2
 # training params
 LOAD = True
 SAVE = True
 VERBOSE = True
-GAME = "Pong-v0"
-GAMMA = 0.98
+GAME = "CartPole-v0"
+GAMMA = 0.95
 RENDER = False
-BATCH_EPISODE = 10
+BATCH_EPISODE = 1
 
 
 class PolicyGradient:
@@ -26,13 +26,18 @@ class PolicyGradient:
             self.sess = tf.Session()
         self.s = tf.placeholder(tf.float32, shape=[None]+input_shape)
         self.a = tf.placeholder(tf.int32, shape=[None, ])
+        self.v = tf.placeholder(tf.int32, shape=[None, ])
         self.a_onehot = tf.one_hot(self.a, output_shape[0])
         self.r = tf.placeholder(tf.float32, shape=[None, ])
-        self.logits, self.probs = self.network(self.s, reuse=False)
-        self.record = {'s': [], 'a': []}
+        self.action_head, self.value_head, self.probs = self.network(
+            self.s, reuse=False)
+        self.record = {'s': [], 'a': [], 'v': []}
         nlogprobs = tf.nn.softmax_cross_entropy_with_logits_v2(
-            labels=self.a_onehot, logits=self.logits)
-        loss = tf.reduce_mean(nlogprobs * self.r)
+            labels=self.a_onehot, logits=self.action_head)
+        policy_loss = tf.reduce_sum(nlogprobs * self.r)
+        value_loss = tf.losses.absolute_difference(
+            self.v, self.value_head)
+        loss = policy_loss+value_loss
         self.train_op = tf.train.AdamOptimizer(
             learning_rate=learning_rate).minimize(loss)
         self.sess.run(tf.global_variables_initializer())
@@ -42,43 +47,36 @@ class PolicyGradient:
             x = tf.layers.flatten(self.s)
             x = tf.layers.dense(
                 x, hidden_units, activation='relu')
-            x = tf.layers.dense(
-                x, output_shape[0], activation='linear')  # logits
-            probs = tf.nn.softmax(x)
-            return x, probs
+            action_head = tf.layers.dense(
+                x, output_shape[0], activation='linear')
+            value_head = tf.reshape(tf.layers.dense(
+                x, 1, activation='linear'), [-1])
+            probs = tf.nn.softmax(action_head)
+            return action_head, value_head, probs
 
     def run(self, states):
         self.record['s'].append(states[0])
-        probs = self.sess.run(self.probs, feed_dict={self.s: states})
+        value, probs = self.sess.run(
+            [self.value_head, self.probs], feed_dict={self.s: states})
         a = np.random.choice(range(probs.shape[1]), size=None, p=probs.ravel())
         self.record['a'].append(a)
+        self.record['v'].append(value)
         return a
 
     def train(self, merged=None):
         # print(self.record['r'].shape, self.record['a'][0])
+        reward = self.record['r'] - np.array(self.record['v']).reshape([-1])
         self.sess.run(self.train_op, feed_dict={
-                      self.s: self.record['s'], self.a: self.record['a'], self.r: self.record['r']})
+                      self.s: self.record['s'], self.a: self.record['a'], self.v: self.record['r'], self.r: reward})
         self.record['s'].clear()
         self.record['a'].clear()
+        self.record['v'].clear()
 
     def load(self, game):
         pass
 
     def save(self, game):
         pass
-
-
-def prepro(I):
-    """ 
-    prepro 210x160x3 uint8 frame into (80x80)
-    from https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5
-    """
-    I = I[35:195]  # crop
-    I = I[::2, ::2, 0]  # downsample by factor of 2
-    I[I == 144] = 0  # erase background (background type 1)
-    I[I == 109] = 0  # erase background (background type 2)
-    I[I != 0] = 1  # everything else (paddles, ball) just set to 1
-    return I
 
 
 def discount_rewards(r):
@@ -88,11 +86,10 @@ def discount_rewards(r):
     """
     discounted_r = np.zeros_like(r)
     running_add = 0
-    for t in reversed(range(0, r.size)):
-        if r[t] != 0:
-            running_add = 0
+    for t in reversed(range(len(r))):
         running_add = running_add * GAMMA + r[t]
         discounted_r[t] = running_add
+
     return (discounted_r - np.mean(discounted_r)) / np.std(discounted_r)
 
 
@@ -103,6 +100,8 @@ def main():
     if LOAD:
         model.load(GAME)
     env = gym.make(GAME)
+    env = env.unwrapped
+    env.seed(1)
     writer = tf.summary.FileWriter("logdir", sess.graph)
     score_ph = tf.placeholder(tf.int16)
     tf.summary.scalar("score", score_ph)
@@ -110,30 +109,24 @@ def main():
         tf.summary.histogram(var.name, var)
     merged = tf.summary.merge_all()
     tf.get_default_graph().finalize()
-    observation = env.reset()
+    s = env.reset()
     rewards = []
     episode_number = 0
     positive_score = 0
     render = RENDER
-    prev_s = None
     while True:
         if render or episode_number % 50 == 49:
             env.render()
-        if prepro is not None:
-            cur_s = prepro(observation).reshape(input_shape)
-        s = cur_s - prev_s if prev_s is not None else np.zeros(input_shape)
-        prev_s = cur_s
         s = s.reshape([1]+input_shape)
         a = model.run(s)
-        observation, reward, done, _ = env.step(a+2)
+        s, reward, done, _ = env.step(a)
         rewards.append(reward)
         if reward == 1:
-            print("!!!!!")
             positive_score += 1
         if done:
             episode_number += 1
-            if np.sum(rewards) > 10 * BATCH_EPISODE:
-                render = True
+            # if np.sum(rewards) > 16:
+            #     render = True
             if episode_number % BATCH_EPISODE == 0:
                 epr = discount_rewards(np.float32(rewards))
                 model.record['r'] = epr
@@ -143,11 +136,12 @@ def main():
                                    score_ph: positive_score})
                 writer.add_summary(summary, episode_number)
                 positive_score = 0
+                # print(model.record)
                 model.train()
                 if SAVE:
                     model.save(GAME)
-                rewards.clear()
-            observation = env.reset()
+            rewards.clear()
+            s = env.reset()
 
 
 if __name__ == "__main__":
